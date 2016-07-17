d1706b0e3e8df5aa34893bfb2527d6d4bc7877c6
Andrey Sokolov <keremet@solaris.kirov.ru>
2016-07-17 22:43:45 +0300

    Замена GET на POST в API

--- pr/api/add_price.php
+++ pr/api/add_price.php
@@ -1,7 +1,7 @@
 <?php
 include('../template/connect.php');
 $stmt = $db->prepare("INSERT INTO pr_product_offers (product,shop,price,creator,date_buy) VALUES (?, ?, ?, ?, CURDATE())");
-if($stmt->execute(array($_GET['product_id'], $_GET['shop_id'], $_GET['price'], $_GET['token']))){
+if($stmt->execute(array($_POST['product_id'], $_POST['shop_id'], $_POST['price'], $_POST['token']))){
 	echo json_encode(array('res' => 'OK'));
 }else{
 	echo json_encode(array('res' => 'Error '.$stmt->errorInfo()[2]));
--- pr/api/login.php
+++ pr/api/login.php
@@ -1,10 +1,14 @@
 <?php
 include('../template/connect.php');
 $stmt = $db->prepare("SELECT id FROM pr_users where login = ? and password = ?");
-$stmt->execute(array($_GET['login'], $_GET['password']));	
+$stmt->execute(array($_POST['login'], $_POST['password']));	
 if($token = $stmt->fetchColumn()){
 	echo json_encode(array('token' => $token));
 }else{
 	echo json_encode(array('error' => 'User not found'));
 }
+/*
+ * sha256, токен 20 символов(буквы, цифры разного регистра)
+ * Инфа о пользователях(ФИО)
+ * */
 ?>
