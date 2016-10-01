1abe5f1ecd95022229616846526accd66f199cb8
Andrey Sokolov <keremet@solaris.kirov.ru>
2016-10-01 10:18:43 +0300

    Добавлен расчет стоимости при вводе данных

--- pr/smart_form/index.php
+++ pr/smart_form/index.php
@@ -5,6 +5,42 @@ include '../template/jstree/jstree.php';
 ?>
 <script type="text/javascript" src="../template/input_calendar/tcal.js"></script>
 <script>
+	(function() {
+	  /**
+	   * Корректировка округления десятичных дробей.
+	   *
+	   * @param {String}  type  Тип корректировки.
+	   * @param {Number}  value Число.
+	   * @param {Integer} exp   Показатель степени (десятичный логарифм основания корректировки).
+	   * @returns {Number} Скорректированное значение.
+	   */
+	  function decimalAdjust(type, value, exp) {
+		// Если степень не определена, либо равна нулю...
+		if (typeof exp === 'undefined' || +exp === 0) {
+		  return Math[type](value);
+		}
+		value = +value;
+		exp = +exp;
+		// Если значение не является числом, либо степень не является целым числом...
+		if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+		  return NaN;
+		}
+		// Сдвиг разрядов
+		value = value.toString().split('e');
+		value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+		// Обратный сдвиг
+		value = value.toString().split('e');
+		return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+	  }
+
+	  // Десятичное округление к ближайшему
+	  if (!Math.round10) {
+		Math.round10 = function(value, exp) {
+		  return decimalAdjust('round', value, exp);
+		};
+	  }
+	})();	
+	
 	function get_price(shop, product){
 		$('.result').html('Поиск цены');
 		jQuery.ajax({
@@ -43,6 +79,12 @@ include '../template/jstree/jstree.php';
 		}
 
 	}
+	function calc_cost(){
+		priceStr = document.getElementById('offer_price').value.replace(',', '.');
+		amountStr = document.getElementById('amount').value.replace(',', '.');
+		
+		$('.result').text('Стоимость='+Math.round10(Number(priceStr)*Number(amountStr), -2));
+	}
 	function SavePrice() {
 		$('.result').html('');
 		jQuery.ajax({
@@ -72,9 +114,9 @@ include '../template/jstree/jstree.php';
 			<h2 style="display: inline;">Дата покупки: </h2>
 			<input size=10 type="text" readonly class="tcal" id="date_buy" value="<?=date("d/m/Y")?>">
 			<h2 style="display: inline;">Цена</h2>
-			<input size=8 onkeypress="$('.result').text('');" type="text" id="offer_price">
+			<input size=8 onkeyup="calc_cost()" type="text" id="offer_price">
 			<h2 style="display: inline;">Количество</h2>
-			<input size=5 onkeypress="$('.result').text('');" type="text" id="amount">
+			<input size=5 onkeyup="calc_cost()" type="text" id="amount">
 			<input type="submit" value="Добавить" onclick="SavePrice(); return false;">
 			<div class="result"></div>
 		</span>
