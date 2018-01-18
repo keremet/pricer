9feafee189f6ed65f72f71039641f8a37431b875
Hrombel <admin@hrombel.ru>
2018-01-18 14:57:53 +0300

    Добавлена форма работы с таблицей трансляции

---
+++ pr/products/translation_table.php
@@ -0,0 +1,308 @@
+<?
+
+define('OK', "{\"status\":\"OK\"}");
+define('ERROR', "{\"error\":\"ERROR\"}");
+
+include '../template/connect.php';
+
+if(isset($_POST['operation']))
+{
+	$op = $_POST['operation'];
+	
+	if($op == 'get') {
+		
+		$id = isset($_POST['cl_id']) ? $_POST['cl_id'] : false;
+		$stmt = $db->prepare('SELECT `a`.`id` AS `id`, 
+						   `a`.`prod_id` AS `cl_id`, 
+						   `a`.`name` AS `name`, 
+						   `a`.`inn` AS `inn`,
+						   `b`.`name` AS `cl_name`
+					FROM `pr_ids` `a`
+					JOIN `pr_consumption_clsf` `b` ON `a`.`prod_id` = `b`.`id`
+					WHERE ' . ($id ? "`a`.`prod_id` = ?" : '1'));
+		$params = array();
+		if($id)
+			$params[] = $id;
+					
+		if($stmt->execute($params)) {
+			
+			$n = $stmt->rowCount();
+			$arr = array();
+			foreach($stmt as $row) {
+				$arr[] = array('id' => $row['id'],
+							   'cl_id' => $row['cl_id'],
+							   'name' => $row['name'],
+							   'inn' => $row['inn'],
+							   'cl_name' => $row['cl_name']);
+			}
+			
+			print json_encode($arr);
+		}
+		else
+			print ERROR;
+	
+	}
+	else if($op == 'get_path') {
+		
+		if(isset($_POST['id'])) {
+			$id = $_POST['id'];
+			$stmt = $db->prepare('select `pr_get_product_path`(?) AS `path`');
+			
+			if($stmt->execute(array($id))) {
+				$path = $stmt->fetch()['path'];
+				if(empty($path))
+					print '"path":[]';
+				else {
+					$path .= $id;
+					$arr = explode('/', $path);
+					
+					$pathArr = array('path' => $arr);
+					print json_encode($pathArr);
+				}
+			}
+			else
+				print ERROR;
+		}
+		else
+			print ERROR;
+		
+	}
+	else if($op == 'item_remove') {
+		
+		if(isset($_POST['id'])) {
+			
+			$id = $_POST['id'];
+			$stmt = $db->prepare('DELETE FROM `pr_ids` WHERE id=?');
+			
+			if($stmt->execute(array($id)))
+				print OK;
+			else
+				print ERROR;
+		}
+		else
+			print ERROR;
+	}
+	else if($op == 'item_add') {
+		
+		if(isset($_POST['id']) && isset($_POST['name']) && isset($_POST['inn'])) {
+			$id = (int)$_POST['id'];
+			$name = $_POST['name'];
+			$inn = $_POST['inn'];
+			
+			$stmt = $db->prepare('INSERT INTO `pr_ids` (`prod_id`, `name`, `inn`) VALUES (?, ?, ?)');
+			$exec_prms = array($_POST['id'], $_POST['name'], $_POST['inn']);
+			
+			if($stmt->execute($exec_prms))
+				print OK;
+			else
+				print ERROR;
+		}
+		else
+			print ERROR;
+		
+	}
+	else
+		print ERROR;
+	
+	exit;
+}
+
+include '../template/header.php';
+include '../template/oft_table.php';
+include '../template/jstree/jstree.php';
+
+headerOut('Соответствие товара', array('prod'));
+
+?>
+
+<table>
+<tr>
+<td valign="top">
+
+<?
+putTree('prod', '');
+?>
+
+</td>
+<td valign="top">
+<?
+	if($_SESSION['user']['id']) {
+		
+		oftTable::init('Выбранный товар:', 'selectedItemTable');
+
+		oftTable::header(array('ИНН', 'Наименование', 'Товар(Классификатор)', 'Действия'));
+		oftTable::end();
+		
+		oftTable::init('Соответствие товара', 'itemsTable');
+
+		oftTable::header(array('ИНН', 'Наименование', 'Товар(Классификатор)', 'Действия'));
+		oftTable::end();
+?>
+
+</td>
+</tr>
+</table>
+
+<script type="text/javascript">
+
+	var table, upTable;
+	var data;
+	var curNodeId;
+
+	function tableRefresh() {
+		
+		jQuery.ajax({
+			url:     'translation_table.php', //Адрес подгружаемой страницы
+			type:     "POST", //Тип запроса
+			dataType: "html", //Тип данных
+			data: {operation:'get'}, 
+			success: function(result) {
+				if(result) {
+					data = JSON.parse(result);
+					if(data.error) {
+						alert("Ошибка выполнения запроса");
+						return;
+					}
+					
+					while(table.rows.length > 1)
+						table.deleteRow(1);
+					for(var i = 0; i < data.length; i++) {
+						var d = data[i];
+						var row = table.insertRow();
+						var cell = row.insertCell();
+						cell.innerHTML = d.inn;
+						
+						cell = row.insertCell();
+						cell.innerHTML = d.name;
+						
+						cell = row.insertCell();
+						cell.innerHTML = "<a href=\"#\" onclick=\"showTreeItem('" + d.cl_id + "')\">" + d.cl_name + "</a>";
+						
+						cell = row.insertCell();
+						cell.innerHTML = "<input type='submit' value='Удалить' onclick='removeRule(" + d.id + ")'>";
+					}
+				}
+				else
+					alert("Ошибка загрузки товаров");
+			}
+		});
+		
+	}
+	
+	function showTreeItem(id) {
+		var inst = $.jstree.reference("#treeprod");
+		var node = inst.get_node(id);
+		if(node === false) {
+			curNodeId = id;
+			jQuery.ajax({
+			url:     'translation_table.php', //Адрес подгружаемой страницы
+			type:     "POST", //Тип запроса
+			dataType: "html", //Тип данных
+			data: {operation:'get_path', id: id}, 
+			success: function(result) {
+				var path = JSON.parse(result);
+				if(path.error) {
+					alert("Ошибка выполнения запроса");
+					return;
+				}
+				path = path.path;
+				var n = path.length - 1;
+				for(var i = 0; i <= n; i++) {
+					node = inst.get_node(path[i]);
+					if(node) {
+						if(i != n && !node.state.opened) {
+							inst.open_node(node, function(obj) {
+								if(obj && id == curNodeId)
+									showTreeItem(id);
+							});
+							return;
+						}
+					}
+					else {
+						alert("Товар не найден");
+						return;
+					}
+				}
+			}
+			});
+		}
+		else {
+			if(!node.state.selected) {
+				inst.deselect_all();
+				inst.select_node(node);
+			}
+		}
+	}
+	
+	function removeRule(id) {
+		
+		jQuery.ajax({
+			url:     'translation_table.php', //Адрес подгружаемой страницы
+			type:     "POST", //Тип запроса
+			dataType: "html", //Тип данных
+			data: {operation:'item_remove', id: id}, 
+			success: function(result) {
+				if(result && JSON.parse(result).status == "OK") {
+					tableRefresh();
+					product_select(id);
+				}
+				else
+					alert("Ошибка удаления элемента");
+			}
+		});
+		
+	}
+	
+	function product_select(id) {
+		
+		jQuery.ajax({
+			url:     'translation_table.php', //Адрес подгружаемой страницы
+			type:     "POST", //Тип запроса
+			dataType: "html", //Тип данных
+			data: {operation:'get', cl_id: id}, 
+			success: function(result) {
+				if(result) {
+					data = JSON.parse(result);
+					if(data.error) {
+						alert("Ошибка выполнения запроса");
+						return;
+					}
+					
+					while(upTable.rows.length > 1)
+						upTable.deleteRow(1);
+					for(var i = 0; i < data.length; i++) {
+						var d = data[i];
+						var row = upTable.insertRow();
+						var cell = row.insertCell();
+						cell.innerHTML = d.inn;
+						
+						cell = row.insertCell();
+						cell.innerHTML = d.name;
+						
+						cell = row.insertCell();
+						cell.innerHTML = "<a href=\"#\" onclick=\"showTreeItem('" + d.cl_id + "')\">" + d.cl_name + "</a>";
+						
+						cell = row.insertCell();
+						cell.innerHTML = "<input type='submit' value='Удалить' onclick='removeRule(" + d.id + ")'>";
+					}
+				}
+				else
+					alert("Ошибка загрузки товаров");
+			}
+		});
+		
+	}
+	
+	$(document).ready(function() {
+		table = document.getElementById('itemsTable');
+		upTable = document.getElementById('selectedItemTable');
+		tableRefresh();
+	});
+	
+</script>
+
+
+<? 
+	}else
+		print "Необходима авторизация";
+include('../template/footer.php'); ?>
