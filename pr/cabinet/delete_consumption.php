92b8a2e8252c212c4c3f96cc66b74b040edbce02
Andrey Sokolov <keremet@solaris.kirov.ru>
2019-03-08 16:43:21 +0300

    Реализована проверка права на удаление расхода

--- pr/cabinet/delete_consumption.php
+++ pr/cabinet/delete_consumption.php
@@ -7,6 +7,42 @@ if($_SESSION['user_id']==null)
 	die('Требуется авторизация');
 
 if (isset($_REQUEST['id'])) {
+    if ($_SESSION['user_del_anothers_consumptions'] != '1') {
+        $stmtS = $db->prepare(
+            "SELECT cc.creator, cc.id_hi
+            FROM ".DB_TABLE_PREFIX."consumption c
+              JOIN ".DB_TABLE_PREFIX."consumption_clsf cc ON cc.id = c.clsf_id
+            WHERE c.id = ?"
+            );
+        if (!$stmtS->execute(array($_REQUEST['id']))) {
+            echo 'Ошибка запроса поиска расхода'; print_r($stmtS->errorInfo());
+            exit();
+        }
+        if ($row = $stmtS->fetch()) {
+            while (is_null($row['creator'])) {
+                $stmtS2 = $db->prepare(
+                    "SELECT cc.creator, cc.id_hi
+                    FROM ".DB_TABLE_PREFIX."consumption_clsf cc
+                    WHERE cc.id = ?"
+                    );
+                if (!$stmtS2->execute(array($row['id_hi']))) {
+                    echo 'Ошибка запроса поиска категории расхода'; print_r($stmtS2->errorInfo());
+                    exit();
+                }
+                if (!($row = $stmtS2->fetch())) {
+                    echo 'Ошибка поиска категории расхода'; print_r($stmtS2->errorInfo());
+                    exit();
+                }
+            }
+            
+            if ($row['creator'] != $_SESSION['user_id'])
+                die("Удаление чужих расходов запрещено для пользователя");
+        } else {
+            echo 'Ошибка поиска расхода'; print_r($stmt->errorInfo());
+            exit();
+        }
+    }
+
 	$stmt = $db->prepare("DELETE FROM ".DB_TABLE_PREFIX."consumption WHERE id = ?");
 	if(!$stmt->execute(array($_REQUEST['id']))){
 		echo 'Ошибка удаления расхода'; print_r($stmt->errorInfo());
