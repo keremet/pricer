c50c77af40ad3d490deb6874613348ce33f46afb
Andrey Sokolov <keremet@solaris.kirov.ru>
2019-03-09 20:10:53 +0300

    Исправлена сортировка чисел на странице аналитики

--- pr/template/js/default.js
+++ pr/template/js/default.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
 	//$('.fancybox').fancybox();
-	$(".tablesorter").tablesorter({sortList: [[0,1]]});
+	$(".tablesorter").tablesorter(
+		{sortList: [[0,1]],
+		headers: {
+		  1: {
+			sorter: "nums"
+		  },
+		  2: {
+			sorter: "digit"
+		  },
+		  3: {
+			sorter: "digit"
+		  }
+		}}
+	);
 });
-
-
 
