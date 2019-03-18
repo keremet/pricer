4e4d428e24f2cbbc28fff455168b24d36a5630e1
Andrey Sokolov <keremet@solaris.kirov.ru>
2019-03-19 01:16:41 +0300

    Убрана буферизация в скрипте резервного копирования

--- pr/cabinet/backup_db.php
+++ pr/cabinet/backup_db.php
@@ -12,79 +12,68 @@
     $stmt = $db->prepare("SHOW TABLES");
     $stmt->execute();
 
-    $index = 0;
     while($result = $stmt->fetchColumn())
     {
         if (preg_match("/^".DB_TABLE_PREFIX.".*/m", $result))
         {
-            $table_name[$index] = $result;
-            $index++;
+            $table_name[] = $result;
         }
     }
 
-    $query = array();
     $count_col = 0;
     foreach($table_name as $name)
     {
+        $stmtS = $db->prepare("SELECT * FROM $name");
+        $stmtS->execute();
+        if($stmtS->rowCount() == 0)
+            continue;
+        
         $stmt = $db->prepare("DESCRIBE $name");
         $stmt->execute();
         $index_add_col = 0;
         $count_row = $stmt->rowCount();
-        $query["$name"] .= "INSERT INTO `$name` (";
+        echo "INSERT INTO `$name` (";
         while($result = $stmt->fetchColumn())
         {
             if($index_add_col != $count_row - 1)
             {
-                $query["$name"] .= $result .= ", ";
+                echo $result . ", ";
             }
             else
             {
-                $query["$name"] .= $result .= ") VALUES " . PHP_EOL;
+                echo $result . ") VALUES " . PHP_EOL;
             }
             $index_add_col++;
         }
 
-        $stmt = $db->prepare("SELECT * FROM $name");
-        $stmt->execute();
-        $result = $stmt->fetchAll();
-        $all_col_count = $stmt->columnCount();
-        $all_row_count = $stmt->rowCount();
-        
-        if($all_row_count == 0)
-        {
-            $query["$name"] = "";
-        }
-
+        $all_col_count = $stmtS->columnCount();
+        $result = $stmtS->fetchAll();
         foreach($result as $key =>  $value)
         {
-            $query["$name"] .= "(";
+            echo "(";
             foreach($value as $k => $val)
             {
                 $data_row = is_null($val) == true ? "null" : "'" . $val . "'";
                 if($count_col == $all_col_count - 1)
                 {
-                    $query["$name"] .= $data_row;
+                    echo $data_row;
                     $count_col = 0;
                 }
                 else
                 {
-                    $query["$name"] .= $data_row . ", " ;
+                    echo $data_row . ", " ;
                     $count_col++;
                 }
 
             }
             if($key == count($result) - 1)
             {
-                $query["$name"] .= ");" . PHP_EOL;
+                echo ");" . PHP_EOL;
             }
             else
             {
-                $query["$name"] .= ")," . PHP_EOL;
+                echo ")," . PHP_EOL;
             }
         }
-    }
-    foreach($query as $val)
-    {
-        echo $val;
     }
 ?>
