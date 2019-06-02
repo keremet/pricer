96d3e8c8e45c3bc65f2fce399ddc0c1b61e379ca
keremet <keremet@solaris.kirov.ru>
2019-06-02 20:09:57 +0300

    Покупки по данным из чеков

--- pr/cabinet/index.php
+++ pr/cabinet/index.php
@@ -207,6 +207,7 @@ headerOut('Личный кабинет');
 			<?}?>
 		</div><br>
 		<a href="rep_actual.php">Отчет по актуальности цен</a><br>
+		<a href="my_purchases.php">Покупки по данным из чеков</a><br>
 		<a href="my_tip.php?user_id=<?=$_SESSION['user_id']?>">Подсказки для меня</a><br>
 		<a href=my_economy.php>Отчет по экономии</a><br>
 		<a href=profile.php>Профилирование</a><br>
---
+++ pr/cabinet/my_purchases.php
@@ -0,0 +1,28 @@
+<?php session_start();?>
+<head>
+	<meta http-equiv="CONTENT-TYPE" content="text/html; charset=UTF-8">
+	<title>Покупки по данным из чеков</title>
+</head>
+<?php
+	include "../template/oft_table.php";
+	include "../template/connect.php";
+	include "../receipts/money_out.php";
+
+	oftTable::init("Покупки по данным из чеков");
+	oftTable::header(array("Дата", "ИНН магазина", "Название магазина", "Адрес магазина", "Товар", "Цена", "Количество"));
+	$stmt = $db->prepare(
+        "SELECT DATE_FORMAT(r.dateTime, '%d-%m-%Y %H:%i:%s') dt, r.userInn, r.user, r.retailPlaceAddress, i.name, i.price, i.quantity
+	     FROM pricer_receipt r 
+		 JOIN pricer_receipt_item i ON i.receipt_id = r.id
+		 WHERE r.user_id=?
+		 ORDER BY r.dateTime desc
+        ");
+	$stmt->execute(array($_SESSION['user_id']));
+	while ($row = $stmt->fetch()) {
+		oftTable::row(array($row['dt'], $row['userInn'], $row['user'], $row['retailPlaceAddress'], $row['name'], money_out($row['price']), $row['quantity']));
+	}
+        
+	oftTable::end();
+?>
+</body>
+</html>
