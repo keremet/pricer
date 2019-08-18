6a2e6f69a9a61894c8504b46b738207198aeb561
keremet <keremet@solaris.kirov.ru>
2019-08-18 20:33:55 +0300

    Новый поиск соответствий магазинов и товаров в чеке и БД

--- pr/cabinet/my_purchases_csv.php
+++ pr/cabinet/my_purchases_csv.php
@@ -8,17 +8,22 @@
 	include "../template/connect.php";
 	include "../receipts/money_out.php";
 
-	echo "Дата|ИНН магазина|Название магазина|Адрес магазина|Товар|Цена|Количество\n";
+	echo "Дата|fiscalDriveNumber|Название магазина|Адрес магазина|Название магазина в чеке|Адрес магазина в чеке|Товар|Единица измерения|В упаковке|Товар в чеке|Цена|Количество\n";
 	$stmt = $db->prepare(
-        "SELECT DATE_FORMAT(r.dateTime, '%d.%m.%Y %H:%i:%s') dt, r.userInn, r.user, r.retailPlaceAddress, i.name, i.price, i.quantity
+        "SELECT DATE_FORMAT(r.dateTime, '%d.%m.%Y %H:%i:%s') dt, r.fiscalDriveNumber, s.name s_name, s.address, r.user, r.retailPlaceAddress, p.name p_name, e.name e_name, p.in_box, i.name, i.price, i.quantity
 	     FROM pricer_receipt r 
+	     LEFT JOIN pricer_fdn_to_shop f2s ON f2s.fiscalDriveNumber = r.fiscalDriveNumber
+	     LEFT JOIN pricer_shops s ON s.id = f2s.shop_id
 		 JOIN pricer_receipt_item i ON i.receipt_id = r.id
+		 LEFT JOIN pricer_receipt_item_name_to_product in2p ON in2p.name = i.name
+		 LEFT JOIN pricer_products p ON p.id = in2p.product_id
+		 LEFT JOIN pricer_ed_izm e ON e.id = p.ed_izm_id
 		 WHERE r.user_id=?
 		 ORDER BY r.dateTime desc
         ");
 	$stmt->execute(array($_SESSION['user_id']));
 	while ($row = $stmt->fetch()) {
-		echo $row['dt'].'|'.$row['userInn'].'|'.$row['user'].'|'.$row['retailPlaceAddress'].'|'.$row['name'].'|'
+		echo $row['dt'].'|'.$row['fiscalDriveNumber'].'|'.$row['s_name'].'|'.$row['address'].'|'.$row['user'].'|'.$row['retailPlaceAddress'].'|'.$row['p_name'].'|'.$row['e_name'].'|'.$row['in_box'].'|'.$row['name'].'|'
 			.str_replace('.', ',', money_to_str($row['price'])).'|'
 			.str_replace('.', ',', $row['quantity'])."\n";
 	}
--- sql/receipt_ddl.sql
+++ sql/receipt_ddl.sql
@@ -3,7 +3,7 @@ CREATE TABLE `pr_receipt` (
   `buyerAddress` text,
   `totalSum` int(11) NOT NULL,
   `addressToCheckFiscalSign` text,
-  `fiscalDriveNumber` varchar(100) NOT NULL,
+  `fiscalDriveNumber` bigint NOT NULL,
   `rawData` text,
   `kktRegId` text,
   `user` text,
@@ -30,7 +30,8 @@ CREATE TABLE `pr_receipt` (
   `rawReceipt` text,
   `checked` BOOLEAN NOT NULL DEFAULT FALSE,
    CONSTRAINT `pr_receipt_uk` UNIQUE KEY (`dateTime`, `totalSum`, `fiscalDriveNumber`, `fiscalDocumentNumber`, `fiscalSign`),
-   CONSTRAINT `pr_receipt_user_id` FOREIGN KEY (`user_id`) REFERENCES `pr_users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+   CONSTRAINT `pr_receipt_user_id` FOREIGN KEY (`user_id`) REFERENCES `pr_users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+   CONSTRAINT `pr_receipt_ins_user_id` FOREIGN KEY (`ins_user_id`) REFERENCES `pr_users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
@@ -69,6 +70,11 @@ CREATE TABLE `pr_receipt_to_shop` (
   CONSTRAINT `pr_receipt_to_shop__user_id` FOREIGN KEY (`user_id`) REFERENCES `pr_users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE `pr_fdn_to_shop` (
+  `fiscalDriveNumber` bigint PRIMARY KEY,
+  `shop_id` int(11) NOT NULL,
+  CONSTRAINT `pr_fdn_to_shop__shop_id` FOREIGN KEY (`shop_id`) REFERENCES `pr_shops` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `pr_receipt_item_to_product` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -79,6 +85,14 @@ CREATE TABLE `pr_receipt_item_to_product` (
   UNIQUE KEY `name_inn` (`inn`,`name`) USING BTREE,
   CONSTRAINT `pr_receipt_item_to_product__product_id` FOREIGN KEY (`product_id`) REFERENCES `pr_products` (`id`),
   CONSTRAINT `pr_receipt_item_to_product__user_id` FOREIGN KEY (`user_id`) REFERENCES `pr_users` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `pr_receipt_item_name_to_product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `product_id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  UNIQUE KEY `pr_receipt_item_name_to_product__name` (`name`) USING BTREE,
+  CONSTRAINT `pr_receipt_item_name_to_product__product_id` FOREIGN KEY (`product_id`) REFERENCES `pr_products` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `pr_receipt_user` (
