f389542db7c06cd349c1d6b648a113f02fe1a500
keremet <keremet@solaris.kirov.ru>
2019-08-22 20:24:06 +0300

    Пополнен список привязок магазинов

--- sql/settings.sql
+++ sql/settings.sql
@@ -1,9 +1,23 @@
 insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9286000100189365, 1);
-insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9252440300027497, 54);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9289000100549551, 1);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9286000100132850, 1);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9286000100192920, 1);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (8712000101024865, 1);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (8710000101009959, 1);
 insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9282000100314611, 2);
 insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9282000100314625, 2);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9282000100314572, 2);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (8710000100446302, 2);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (8710000100446352, 2);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (8710000100446602, 2);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (8710000100448250, 2);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9252440300027497, 54);
 insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (8711000101957785, 55);
 insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9280440300226892, 56);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9287440300015213, 57);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9287440300140176, 58);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9282000100049387, 59);
+insert into pricer_fdn_to_shop(fiscalDriveNumber, shop_id) values (9282000100049467, 60);
 
 
 insert into pricer_receipt_item_name_to_product(name, product_id) values ('Творог Вятушка 9% весовой, упак (8 кг))', 231);
