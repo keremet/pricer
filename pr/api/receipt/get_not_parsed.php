06422912e637d246eb1616bb6524f8798efd1750
keremet <keremet@solaris.kirov.ru>
2019-12-25 22:37:45 +0300

    Первыми извлекаются чеки тех, кто указал свою учетку

--- pr/api/receipt/get_not_parsed.php
+++ pr/api/receipt/get_not_parsed.php
@@ -2,12 +2,13 @@
 include('../../template/connect.php');
 
 foreach($db->query(
-   "SELECT id, checked, if(rawReceipt is Null or rawReceipt='', '0', '1') rawLoaded
+   "SELECT id, checked, if(rawReceipt is Null or rawReceipt='', '0', '1') rawLoaded, if(exists(SELECT 1 FROM ".DB_TABLE_PREFIX."receipt_user u where u.user_id=r.user_id),1,0) u_acc_exists
 	FROM ".DB_TABLE_PREFIX."receipt r
 	WHERE NOT EXISTS (
 		SELECT 1 
 		FROM ".DB_TABLE_PREFIX."receipt_item i
 		WHERE i.receipt_id = r.id
-	) AND (rawReceipt is Null or rawReceipt != 'the ticket was not found')") as $row)
+	) AND (rawReceipt is Null or rawReceipt != 'the ticket was not found')
+	ORDER BY u_acc_exists DESC, id") as $row)
   echo $row['id']." ".$row['checked']." ".$row['rawLoaded']."\n";
 ?>
