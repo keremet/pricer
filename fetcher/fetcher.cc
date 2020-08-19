7d00fc059e3dd8e550b854a412b2c5196860ae9d
Andrey Sokolov <keremet@solaris.kirov.ru>
2020-08-19 22:44:25 +0300

    Исправлен запрос списка челов на извлечение из налоговой

--- fetcher/fetcher.cc
+++ fetcher/fetcher.cc
@@ -21,11 +21,7 @@ int main(void) {
       unique_ptr<sql::ResultSet> res (stmtS->executeQuery(
             "SELECT id, rawReceipt, fiscalDriveNumber, fiscalDocumentNumber, fiscalSign, totalSum, DATE_FORMAT(dateTime, '%d.%m.%Y+%H%%3A%i'), if(rawReceipt is Null or rawReceipt='', '0', '1') rawLoaded, if(exists(SELECT 1 FROM receipt_user u where u.user_id=r.user_id),1,0) u_acc_exists"
             "  FROM receipt r"
-            "  WHERE NOT EXISTS ("
-            "  	 SELECT 1 "
-            "    FROM receipt_item i"
-            "    WHERE i.receipt_id = r.id"
-            "  ) AND (rawReceipt is Null or rawReceipt != 'the ticket was not found')"
+            "  WHERE rawReceipt is Null"
             "  ORDER BY u_acc_exists DESC, id"));
       while (res->next()) {
           cout << "id = " << res->getString(1) << endl;
