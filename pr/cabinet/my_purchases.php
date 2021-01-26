d6dba986d13ce1858ab6b589492c8b2426a4bdc1
Andrey Sokolov <keremet@solaris.kirov.ru>
2021-01-26 23:20:07 +0300

    Добавлена ссылка на чек из отчета по покупкам

--- pr/cabinet/my_purchases.php
+++ pr/cabinet/my_purchases.php
@@ -11,7 +11,7 @@
 	oftTable::init("Покупки по данным из чеков");
 	oftTable::header(array("Дата", "ИНН магазина", "Название магазина", "Адрес магазина", "Товар", "Цена", "Количество"));
 	$stmt = $db->prepare(
-        "SELECT DATE_FORMAT(r.dateTime, '%d-%m-%Y %H:%i:%s') dt, r.userInn, r.user, r.retailPlaceAddress, i.name, i.price, i.quantity
+        "SELECT r.id, DATE_FORMAT(r.dateTime, '%d-%m-%Y %H:%i:%s') dt, r.userInn, r.user, r.retailPlaceAddress, i.name, i.price, i.quantity
 	     FROM receipt r 
 		 JOIN receipt_item i ON i.receipt_id = r.id
 		 WHERE r.user_id=?
@@ -19,7 +19,7 @@
         ");
 	$stmt->execute(array($_SESSION['user_id']));
 	while ($row = $stmt->fetch()) {
-		oftTable::row(array($row['dt'], $row['userInn'], $row['user'], $row['retailPlaceAddress'], $row['name'], money_out($row['price']), $row['quantity']));
+		oftTable::row(array('<a href=../receipts/receipt.php?id='.$row['id'].'>'.$row['dt'].'</a>', $row['userInn'], $row['user'], $row['retailPlaceAddress'], $row['name'], money_out($row['price']), $row['quantity']));
 	}
         
 	oftTable::end();
